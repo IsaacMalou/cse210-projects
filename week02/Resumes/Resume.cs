e591ccfe5a39e655aa557e79322ec35d431d46a5
IsaacMalou <isaacaguerjr@gmail.com>
2026-05-14 23:56:33 +0200

    update

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -5,7 +5,7 @@ public class Resume
 {
     public string _name;
 
-    // Initialize the list immediately so it's ready to use
+
     public List<Job> _jobs = new List<Job>();
 
     public void Display()
@@ -13,7 +13,7 @@ public class Resume
         Console.WriteLine($"Name: {_name}");
         Console.WriteLine("Jobs:");
 
-        // Iterate through each job and call its own Display method
+
         foreach (Job job in _jobs)
         {
             job.Display();
